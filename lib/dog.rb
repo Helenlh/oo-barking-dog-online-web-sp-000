51b8bc0b958d704f3b021bf7cb07df8358f21ba6
HELEN HARMAN <hlharman@bellsouth.net>
2020-12-22 00:49:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Dog 
+  def name= (dog_name)
+  @this_dogs_name 
+end 
+
+def name 
+  @this_dogs_name
+end 
+end 
+
+Fido = Dog.new 
+Fido.name = "Fido"
+puts Fido.name 
+
+class Dog 
+  def bark 
+  puts "woof!"
+end 
+end 
